0399afab155da58c0da293a65c44fd6a6569afd6
nlovas <nlovas@ualberta.ca>
2017-01-22 18:00:18 -0700

    credit and license done

--- README.md
+++ README.md
@@ -7,6 +7,47 @@ See requirements.org (plain-text) for a description of the project.
 
 Make a simple webserver.
 
+Sources and Contributions:
+
+Consulted and pair programmed with Jillian Lovas (CCID jlovas)
+
+Python - What is self:
+Answered by Rohit Jain (http://stackoverflow.com/users/1679863/rohit-jain)
+on StackOverflow (http://stackoverflow.com/a/21695159)
+
+Ryan Satyabrata's code was looked at to help understand what we needed to do and get a grasp on python such as the existence of the os.path library (I did not know python prior to the project)
+(https://github.com/kobitoko/CMPUT404-assignment-webserver)
+on Github (https://github.com/kobitoko)
+
+http headers must end with \r\n:
+Answered by Piskvor (http://stackoverflow.com/users/19746/piskvor)
+on StackOverflow (http://stackoverflow.com/a/5757349)
+
+detecting if a path is a directory or file etc, and info on normpath:
+https://docs.python.org/2/library/os.path.html
+
+get the current directory:
+https://docs.python.org/2/library/os.html#os.curdir
+
+reading a file in python:
+Answered by klaas (http://stackoverflow.com/users/2186599/klaas)
+on StackOverflow (http://stackoverflow.com/a/34307859)
+
+redirection code for HTTP:
+https://en.wikipedia.org/wiki/HTTP_302
+
+get length in bytes:
+Answered by Kris (http://stackoverflow.com/users/3783770/kris)
+on StackOverflow (http://stackoverflow.com/a/30686735)
+
+how to use mimetypes:
+Answered by nosklo (http://stackoverflow.com/users/17160/nosklo)
+on StackOverflow (http://stackoverflow.com/a/947443)
+
+how to call a Python function:
+Answered by Korem(http://stackoverflow.com/users/1809530/korem)
+on StackOverflow (http://stackoverflow.com/a/24291997)
+
 Contributors / Licensing
 ========================
 
@@ -16,6 +57,9 @@ server.py contains contributions from:
 
 * Abram Hindle
 * Eddie Antonio Santos
+* Nicole Lovas
+* Jillian Lovas
+* Ryan Satybrata 
 
 But the server.py example is derived from the python documentation
 examples thus some of the code is Copyright © 2001-2013 Python
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #  coding: utf-8 
 import SocketServer, os.path, mimetypes
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2017 Abram Hindle, Eddie Antonio Santos, Nicole Lovas
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -32,21 +32,25 @@ class MyWebServer(SocketServer.BaseRequestHandler):
 	header = "" #send it to the browser or curler
 
 	def sendPage(self, path):
-			#http://stackoverflow.com/questions/34307339/reading-a-txt-file-from-a-folder-in-python-script
-########cite this better later
-		print "i will send a page"
-#how to use mimetypes http://stackoverflow.com/questions/947372/custom-simple-python-http-server-not-serving-css-files
+
+		#print "i will send a page"
+#how to use mimetypes:
+#Answered by nosklo (http://stackoverflow.com/users/17160/nosklo)
+#on StackOverflow (http://stackoverflow.com/a/947443)
 		mimetype, _ = mimetypes.guess_type(os.path.normpath(os.curdir + path))
 		ctype = 'Content-Type: ' + mimetype + ';\r\n' 
-		print "path: %s" % path[1:]
-		#csize = '\nContent-Length ' + str(os.path.getsize(path[1:])) ###copied function call idea from ryans code
-
-		
-
+		#print "path: %s" % path[1:]
+				
+			#reading a file in python:
+#Answered by klaas (http://stackoverflow.com/users/2186599/klaas)
+#on StackOverflow (http://stackoverflow.com/a/34307859)
 		f = open( os.path.normpath(os.curdir + path), 'r')
 		contents = f.read()
 		f.close()
 		self.header = "HTTP/1.1 200 OK\r\n"
+#get length in bytes:
+#Answered by Kris (http://stackoverflow.com/users/3783770/kris)
+#on StackOverflow (http://stackoverflow.com/a/30686735)
 		csize = 'Content-Length: ' + str(len(contents.encode('utf-8'))) + '\r\n'
 		self.request.sendall(self.header + ctype + csize + '\r\n' + contents) 
 
@@ -54,9 +58,8 @@ class MyWebServer(SocketServer.BaseRequestHandler):
 	def handle(self):
 		self.data = self.request.recv(1024).strip()
 		command = self.data.split()
-		print "your command was %s" % command[0]
-		#print "you wanted %s" % command[1]
-		#print ("Got a request of: %s\n" % self.data)
+		#print "your command was %s" % command[0]
+
 		if command[0] != "GET":
 			#not implemented, send error code in header
 			self.header = "HTTP/1.1 405 Method Not Allowed\r\n"
@@ -64,11 +67,13 @@ class MyWebServer(SocketServer.BaseRequestHandler):
 		
 		else:
 			#get the files from www/
+#this approach was referenced from Ryan Satyabrata (Apache 2.0)
+#https://github.com/kobitoko/CMPUT404-assignment-webserver
+#on Github (https://github.com/kobitoko)
 			path = "/www"
 			path = path + command[1] #get the location specified
 			
-			#print "path: %s " % os.path.normpath(os.curdir + path)
-			print "path: %s " % os.curdir + path
+			#print "path: %s " % os.curdir + path
 
 			#for the 'how secure are you?' test
 			#first check if the end of the path has a /
@@ -88,29 +93,33 @@ class MyWebServer(SocketServer.BaseRequestHandler):
 				#if theres no slash at the end, perform a redirect
 				if( path[-1] != '/'):	
 
-					relocation = path.split('/')			
-					print "redirecting"
-					print "R: path: %s" % path
+								
+					#print "redirecting"
+					#print "R: path: %s" % path
 					self.header = "HTTP/1.1 302 Found\r\n"
-					#learned that I only need to send the latter part of the path from looking at Ryan's code. otherwise the redirect is the incorrect URL
+					#learned that I only need to send the latter part of the path from looking at Ryan Satybrata's code (Apache 2.0) (https://github.com/kobitoko/CMPUT404-assignment-webserver) 
+#on Github (https://github.com/kobitoko) 
+#otherwise the redirect is the incorrect URL
 					self.request.sendall(self.header + "Location: " + path[4:] + '/' + '\r\n');
 				#otherwise, send the appropriate index.html
 				else:
-					print "ok directory"
+					#print "ok directory"
 					path = path + "index.html"
 					self.sendPage(path)
 
 				
 			else:
-				
+				#python documentation for os.path https://docs.python.org/2/library/os.path.html
+				#python documentation for os.curdir https://docs.python.org/2/library/os.html#os.curdir
 				if(os.path.isfile( os.curdir + path) ): #check if this path is a file
-					print "itsa file!"
+					#print "itsa file!"
 
-					self.sendPage(path) #how to call this function http://stackoverflow.com/questions/24291941/python-nameerror-global-name-sortlist-is-not-defined-during-recursion
-
-				#print ("the request was %s\n" % self.data)
+					self.sendPage(path) #how to call a Python function:
+#Answered by Korem(http://stackoverflow.com/users/1809530/korem)
+#on StackOverflow (http://stackoverflow.com/a/24291997)
+				
 				else: #the current path is not a file nor directory
-					print "not a file or directory"
+					#print "not a file or directory"
 					self.header = "HTTP/1.1 404 Page Not Found\r\n"
 					ptype = 'Content-Type: text/html;\r\n'
 					
@@ -118,7 +127,7 @@ class MyWebServer(SocketServer.BaseRequestHandler):
 					psize = 'Content-Length: ' + str(len(page.encode('utf-8'))) + '\r\n'
 					self.request.sendall(self.header + ptype + psize + '\r\n' + page) 
 
-#			self.request.sendall("OK")
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
